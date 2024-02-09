1c3380f8827a335799fc87d253715fec0b8f74e3
Emmanuel Pacaud <emmanuel.pacaud@free.fr>
2024-02-09 19:39:27 +0100

    Use cpp name mangling

--- callbacks.h
+++ callbacks.h
@@ -1,16 +1,8 @@
 #include "TObject.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void URZoomAllXAsLast(TObject* objaxis);
 void poptest1(int a=5, int b = 56);
 void poptest2(int a, int b, TObject* c);
 void poptest2bis(TObject* c);
 int  poptest3();
 void poptest4(const char* ch);
-
-#ifdef __cplusplus
-}
-#endif
